68100199a72ba4c4bcc845822584d2e7ebb8122b
iamSobhan <85999157+iamSobhan@users.noreply.github.com>
2022-01-16 20:04:37 +0530

    comitting a python file

---
+++ File_Handling.py
@@ -0,0 +1,12 @@
+#Example1
+
+#import os
+#file = open("C:/Users/Sobhan/Desktop/demofile.txt", 'w')
+#file.close()
+
+
+#Example2
+
+file = open("C:/Users/Sobhan/Desktop/demofile.txt", 'r')
+print(file.read())
+file.close()
