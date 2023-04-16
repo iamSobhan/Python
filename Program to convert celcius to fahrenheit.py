2399aaa2ac1edeaa498e772648a41de8ac339841
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2023-04-16 14:07:09 +0530

    Add files via upload

--- Program to convert celcius to fahrenheit.py
+++ Program to convert celcius to fahrenheit.py
@@ -1,5 +1,8 @@
-celsius = 37.5
+# take input temperature in Celsius from user
+celsius = float(input("Enter temperature in Celsius: "))
 
+# calculate temperature in Fahrenheit
 fahrenheit = (celsius * 1.8) + 32
 
-print('%0.1f'degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+# display the result
+print("{0} degree Celsius is equal to {1} degree Fahrenheit".format(celsius, fahrenheit))
