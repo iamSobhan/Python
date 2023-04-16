f5dd797964ef2793650d9c326f9ec08210966dc1
Sobhan Maity <85999157+iamSobhan@users.noreply.github.com>
2023-04-16 13:42:48 +0530

    Add files via upload

---
+++ Program to Illustrate Different Set Operations.py
@@ -0,0 +1,11 @@
+M = {0, 4, 5, 6, 8}
+
+N = {9, 7, 3, 2, 1}
+
+print("Union of M and N is: ", M | N)
+
+print("Intersection of M and N is: ", M & N)
+
+print("Difference between M and N is: ", M - N)
+
+print("Symmetric difference of E and N is: ", M ^ N)
